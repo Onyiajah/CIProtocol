f9d7b4a4bf2ff6c9b0224b105e19c72a80da0cab
effulgentstar <ajahlucy@gmail.com>
2025-04-22 05:15:36 +0100

    fix

---
+++ src/assets/images.js
@@ -0,0 +1,70 @@
+// Wallet logos
+import metamask from '/public/assets/images/metamask.png';
+import trustwallet from '/public/assets/images/trustwallet.png';
+import backpackLogo from '/public/assets/images/backpack-logo.png';
+import exodusLogo from '/public/assets/images/exodus-logo.png';
+import fireblocksLogo from '/public/assets/images/fireblocks-logo.png';
+import jupiterLogo from '/public/assets/images/jupiter-logo.png';
+import phantomLogo from '/public/assets/images/phantom-logo.png';
+import coinbaseLogo from '/public/assets/images/coinbase-wallet-logo.png';
+import bifrostLogo from '/public/assets/images/bifrost-logo.png';
+import wemixLogo from '/public/assets/images/wemix-logo.png';
+import softlareLogo from '/public/assets/images/solflare-logo.png';
+import blackfortLogo from '/public/assets/images/blackfort-logo.png';
+import btcLogo from '/public/assets/images/btc-logo.png';
+import cotiLogo from '/public/assets/images/coti-logo.png';
+import cipLogo from '/public/assets/images/logo-1.png';
+
+
+// UI/Icons
+import others from '/public/assets/images/others.png';
+import profile from '/public/assets/images/profile.png';
+import shield from '/public/assets/images/shield.png';
+import tutorialImage from '/public/assets/images/tutorial-image.png';
+import cloud from '/public/assets/images/cloud.png';
+import coins from '/public/assets/images/Coins.png';
+import lightning from '/public/assets/images/Lightning.png';
+import bell from '/public/assets/images/Bell.png';
+import plusIcon from '/public/assets/images/plus-icon.png';
+import house from '/public/assets/images/house.png';
+import dropdown from '/public/assets/images/dropdown.png';
+import header from '/public/assets/images/header.png';
+import howItWorks from '/public/assets/images/how-it-works.png';
+import backgroundImage from '/public/assets/images/background-image.jpeg';
+
+const images = {
+  walletLogos: {    cipLogo,
+    metamask,
+    trustwallet,
+    backpackLogo,
+    exodusLogo,
+    fireblocksLogo,
+    jupiterLogo,
+    phantomLogo,
+    coinbaseLogo,
+    bifrostLogo,
+    wemixLogo,
+    softlareLogo,
+    blackfortLogo,
+    btcLogo,
+    cotiLogo
+  },
+  uiIcons: {
+    others,
+    profile,
+    shield,
+    tutorialImage,
+    cloud,
+    coins,
+    lightning,
+    bell,
+    plusIcon,
+    house,
+    dropdown,
+    header,
+    howItWorks,
+    backgroundImage,
+  },
+};
+
+export default images;
