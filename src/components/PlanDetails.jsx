92d80d0b7c77e12137a71d3ae596348b107a256e
Onyiajah <150726268+Onyiajah@users.noreply.github.com>
2025-04-28 18:52:46 +0100

    Update PlanDetails.jsx

--- src/components/PlanDetails.jsx
+++ src/components/PlanDetails.jsx
@@ -8,10 +8,6 @@ const { walletLogos, uiIcons } = images;
 const {cipLogo} = walletLogos;
 
 const {profile} = uiIcons;
-// Use planName from params if available, otherwise fall back to localStorage
-  const planName = planNameFromParams || localStorage.getItem("userPlan") || "Free";
-  const loggedonuser = localStorage.getItem("emailLoggedIn");
-
 
 function PlanDetails() {
   const navigate = useNavigate();
